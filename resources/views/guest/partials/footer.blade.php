6457bc63d162a09c6996f21f55b337aeb3ce192b
Guz117 <cristian.guzman117@gmail.com>
2022-02-11 16:31:07 +0100

    footer

---
+++ resources/views/guest/partials/footer.blade.php
@@ -0,0 +1,82 @@
+<div class="footer">
+        <div class="footer-contain">
+            <div class="info">
+                <div class="social-info">
+                    <div class="row-left">
+                        <p class="title-info">DC COMICS</p>
+                        <ul class="ul-info">
+                            <li class="list-info"><a href="">Characters</a></li>
+                            <li class="list-info"><a href="">Comics</a></li>
+                            <li class="list-info"><a href="">Movies</a></li>
+                            <li class="list-info"><a href="">TV</a></li>
+                            <li class="list-info"><a href="">Games</a></li>
+                            <li class="list-info"><a href="">Videos</a></li>
+                            <li class="list-info"><a href="">News</a></li>
+                        </ul>
+                        <p class="shop title-info">SHOP</p>
+                        <ul class="ul-info">
+                            <li class="list-info"><a href="">Shop DC</a></li>
+                            <li class="list-info"><a href="">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="row-center">
+                        <p class="title-info">DC</p>
+                        <ul class="ul-info">
+                            <li class="list-info"><a href="">Terms Of Use</a></li>
+                            <li class="list-info"><a href="">Privacy policy (New)</a></li>
+                            <li class="list-info"><a href="">Ad Choices</a></li>
+                            <li class="list-info"><a href="">Advertising</a></li>
+                            <li class="list-info"><a href="">Jobs</a></li>
+                            <li class="list-info"><a href="">Subscriptions</a></li>
+                            <li class="list-info"><a href="">Talent Workshop</a></li>
+                            <li class="list-info"><a href="">CPSC Certificates</a></li>
+                            <li class="list-info"><a href="">Ratings</a></li>
+                            <li class="list-info"><a href="">Shop Help</a></li>
+                            <li class="list-info"><a href="">Contacts</a></li>
+                        </ul>
+                    </div>
+                    <div class="row-right">
+                        <p class="title-info">SITES</p>
+                        <ul class="ul-info">
+                            <li class="list-info"><a href="">DC</a></li>
+                            <li class="list-info"><a href="">MAD Magazine</a></li>
+                            <li class="list-info"><a href="">DC KIds</a></li>
+                            <li class="list-info"><a href="">DC Universe</a></li>
+                            <li class="list-info"><a href="">DC POwer Visa</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="logo-info">
+                </div>
+
+            </div>
+            
+            
+        </div>
+        <div class="footer-social">
+            <div class="social">
+                <span class="sign-up"><a href="">SIGN-UP NOW!</a></span>
+                <div class="footer-follow">
+                    <span class="follows">FOLLOW US</span>
+                    <ul class="ul-social">
+                        <li class="list-social">
+                            <a href=""><img src="{{ asset('images/footer-facebook.png') }}" alt=""></a>
+                        </li> 
+                        <li class="list-social">
+                            <a href=""><img src="{{ asset('images/footer-twitter.png') }}" alt=""></a>
+                        </li>
+                        <li class="list-social">
+                            <a href=""><img src="{{ asset('images/footer-youtube.png') }}" alt=""></a>
+                        </li>
+                        <li class="list-social">
+                            <a href=""><img src="{{ asset('images/footer-pinterest.png') }}" alt=""></a>  
+                        </li>
+                        <li class="list-social">
+                            <a href=""><img src="{{ asset('images/footer-periscope.png') }}" alt=""></a>
+                        </li>  
+                    </ul> 
+                </div>
+            </div>
+        </div>
+    </div>
--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,17 +1,17 @@
 <div class="header">
     <div class="contain">
-        <img src="{{ asset('images/dc-logo.png') }}" alt="">
-        <ul>
-            <li><a class="active" href="">CHARACTERS</a></li>
-            <li><a class="active" href="">COMICS</a></li>
-            <li><a class="active" href="">MOVIES</a></li>
-            <li><a class="active" href="">TV</a></li>
-            <li><a class="active" href="">GAMES</a></li>
-            <li><a class="active" href="">COLLECTIBLES</a></li>
-            <li><a class="active" href="">VIDEOS</a></li>
-            <li><a class="active" href="">FANS</a></li>
-            <li><a class="active" href="">NEWS</a></li>
-            <li><a class="active" href="">SHOP</a></li>
+        <img class="img-header" src="{{ asset('images/dc-logo.png') }}" alt="">
+        <ul class="ul-header">
+            <li class="li-header"><a class="active" href="">CHARACTERS</a></li>
+            <li class="li-header"><a class="active" href="">COMICS</a></li>
+            <li class="li-header"><a class="active" href="">MOVIES</a></li>
+            <li class="li-header"><a class="active" href="">TV</a></li>
+            <li class="li-header"><a class="active" href="">GAMES</a></li>
+            <li class="li-header"><a class="active" href="">COLLECTIBLES</a></li>
+            <li class="li-header"><a class="active" href="">VIDEOS</a></li>
+            <li class="li-header"><a class="active" href="">FANS</a></li>
+            <li class="li-header"><a class="active" href="">NEWS</a></li>
+            <li class="li-header"><a class="active" href="">SHOP</a></li>
         </ul>
     </div>
     <div class="img-jumbotron">
