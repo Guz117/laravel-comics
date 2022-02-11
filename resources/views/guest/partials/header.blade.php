7b20220c5aea1458faf458d2e9b4750c32170bd4
Guz117 <cristian.guzman117@gmail.com>
2022-02-11 21:39:12 +0100

    barra di ricerca

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,20 +1,33 @@
-<div class="header">
-    <div class="contain">
-        <img class="img-header" src="{{ asset('images/dc-logo.png') }}" alt="">
-        <ul class="ul-header">
-            <li class="li-header"><a class="active" href="">CHARACTERS</a></li>
-            <li class="li-header"><a class="active" href="">COMICS</a></li>
-            <li class="li-header"><a class="active" href="">MOVIES</a></li>
-            <li class="li-header"><a class="active" href="">TV</a></li>
-            <li class="li-header"><a class="active" href="">GAMES</a></li>
-            <li class="li-header"><a class="active" href="">COLLECTIBLES</a></li>
-            <li class="li-header"><a class="active" href="">VIDEOS</a></li>
-            <li class="li-header"><a class="active" href="">FANS</a></li>
-            <li class="li-header"><a class="active" href="">NEWS</a></li>
-            <li class="li-header"><a class="active" href="">SHOP</a></li>
-        </ul>
-    </div>
-    <div class="img-jumbotron">
-        <div class="series"><a href="">CURRENT SERIES</a></div>
+<div class="header-tot">
+    <div class="header">
+        <div class="power">
+            <div class="power-dc">
+                <p>DC POWER&#8480;</p>
+                <p>VISA&#174;</p>
+                <p id="additional">ADDITIONAL DC SITES <i class="fa-solid fa-caret-down"></i></p>
+            </div>
+        </div>
+        <div class="contain">
+            <img class="img-header" src="{{ asset('images/dc-logo.png') }}" alt="">
+            <ul class="ul-header">
+                <li class="li-header"><a class="active" href="">CHARACTERS</a></li>
+                <li class="li-header"><a class="active" href="">COMICS</a></li>
+                <li class="li-header"><a class="active" href="">MOVIES</a></li>
+                <li class="li-header"><a class="active" href="">TV</a></li>
+                <li class="li-header"><a class="active" href="">GAMES</a></li>
+                <li class="li-header"><a class="active" href="">COLLECTIBLES</a></li>
+                <li class="li-header"><a class="active" href="">VIDEOS</a></li>
+                <li class="li-header"><a class="active" href="">FANS</a></li>
+                <li class="li-header"><a class="active" href="">NEWS</a></li>
+                <li class="li-header"><a class="active" href="">SHOP</a><i class="fa-solid fa-caret-down"></i></li>
+            </ul>
+            <div class="search">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
+        </div>
+        <div class="img-jumbotron">
+            <div class="series"><a href="">CURRENT SERIES</a></div>
+        </div>
     </div>
 </div>
